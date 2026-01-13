32127d5d5a8a59961ab2613365de84aef2e7e1fc
Sander Theeuwen <theeuwen.sander@gmail.com>
2026-01-13 20:24:44 +0100

    Added years to projects. Added sorting function.

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,57 +1,140 @@
+const projectContainer = document.getElementById("projectCards"); 
+
+function sort()
+{
+    switch (document.getElementById("sort").textContent)
+    {
+        case "A - Z":
+            document.getElementById("sort").textContent = "Z - A";
+            loadProjectCards("name", true);
+            break;
+        case "Z - A":
+            document.getElementById("sort").textContent = "Old - New";
+            loadProjectCards("year", false);
+            break;
+        case "Old - New":
+            document.getElementById("sort").textContent = "New - Old";
+            loadProjectCards("year", true);
+            break;
+        case "New - Old":
+            document.getElementById("sort").textContent = "A - Z";
+            loadProjectCards("name", false);
+            break;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", async function ()
 {
-    const projectContainer = document.getElementById("projectCards");
+    loadProjectCards("year", true);
+});
 
-    async function loadProjectCards()
+async function loadProjectCards(filter, reversed)
+{
+    projectContainer.innerHTML = ``;
+
+    try
     {
-        try
-        {
-            const response = await fetch(`/generated/projects-paths.json`);
-            const projectPaths = await response.json();
+        const response = await fetch(`/generated/projects-paths.json`);
+        const projectPaths = await response.json();
+        const projects = [];
 
-            for (const projectPath of projectPaths)
+        for (const projectPath of projectPaths)
+        {
+            const projectResponse = await fetch(`/${projectPath.path}/project.json`);
+            const projectData = await projectResponse.json();
+            projectData.id = projectPath.path.substring(9);
+
+            // Make project visible if editing and unpublished
+            if (!projectData.published && !window.isLocal)
             {
-                const projectResponse = await fetch(`/${projectPath.path}/project.json`);
-                const projectData = await projectResponse.json();
-                projectData.id = projectPath.path.substring(9);
-
-                // Make project visible if editing and unpublished
-                if (!projectData.published && !window.isLocal)
-                {
-                    continue;
-                }
-                const projectCard = document.createElement("a");
-                projectCard.classList.add("project-card");
-                if (!projectData.published)
-                {
-                    projectCard.classList.add("unpublished");
-                }
-                projectCard.href = `project.html?project_id=${projectData.id}`; // Adjusted for linking
-
-                if (!projectData.image)
-                {
-                    projectCard.innerHTML = `
-                        <h3>${projectData.name}</h3>
-                        <p>${projectData.description}</p>
-                    `;
-                }
-                else
-                {
-                    projectCard.innerHTML = `
-                        <img src="/projects/${projectData.id}/images/${projectData.image}" alt="${projectData.name} picture">
-                        <h3>${projectData.name}</h3>
-                        <p>${projectData.description}</p>
-                    `;
-                }
-
-                projectContainer.appendChild(projectCard);
+                continue;
             }
+
+            projects.push(projectData);
         }
-        catch (error)
+
+        // Sort
+        projects.sort(function(a, b)
         {
-            console.error("Error loading project cards:", error);
+            if (a.name == "Miscellaneous")
+            {
+                return 1;
+            }
+            switch (filter)
+            {
+                case "name":
+                    if (a.name > b.name)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        if (a.name === b.name)
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            return -1;
+                        }
+                    }
+                case "year":
+                    if (a.year > b.year)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        if (a.year === b.year)
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            return -1;
+                        }
+                    }
+            }
+        });
+        if (reversed)
+        {
+            projects.reverse();
+        }
+
+        // Display
+        for (const project of projects)
+        {
+            const projectCard = document.createElement("a");
+            projectCard.classList.add("project-card");
+            if (!project.published)
+            {
+                projectCard.classList.add("unpublished");
+            }
+            projectCard.href = `project.html?project_id=${project.id}`; // Adjusted for linking
+
+            /*
+            <h3>Name</h3>
+            <p>Date</p>
+            <p>Description</p>
+            <img project image>
+            */
+
+            projectCard.innerHTML = `<h3>${project.name}</h3>`;
+            if (project.year && project.year != "-")
+            {
+                projectCard.innerHTML += `<p class="date">${project.year}</p>`;
+            }
+            projectCard.innerHTML += `<p>${project.description}</p>`;
+            if (project.image)
+            {
+                projectCard.innerHTML += `<img src="/projects/${project.id}/images/${project.image}" alt="${project.name} picture">`
+            }
+
+            projectContainer.appendChild(projectCard);
         }
     }
-
-    loadProjectCards();
-});
+    catch (error)
+    {
+        console.error("Error loading project cards:", error);
+    }
+}
